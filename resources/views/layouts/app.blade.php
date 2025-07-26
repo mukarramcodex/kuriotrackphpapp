f7688f0b9339a5b33460dd1d2303a715f73286e8
Mukarram Ali <info.mukarramali@gmail.com>
2025-07-26 08:44:30 +0500

    App Layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,8 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="flex bg-gray-50 text-gray-900">
+            
             @include('layouts.navigation')
 
             <!-- Page Heading -->
